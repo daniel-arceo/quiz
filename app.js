6d8a720760ceca6bef2efe1d58cc76d323be2208
Daniel Arceo <daniel.arceo@incentro.com>
2015-07-02 13:01:34 +0200

    Authentication on editing comments

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var routes = require('./routes/index');
 var methodOverride = require('method-override');
+var session = require("express-session");
 
 var app = express();
 
@@ -21,9 +22,21 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser("Quiz 2015"));
+app.use(session());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+
+    res.locals.session = req.session;
+    next();
+});
+
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,14 +11,19 @@ router.get('/', function(req, res) {
 
 router.param('quizId', quizController.load); //autoload: quizId
 
+//Definicion de rutas de sesion
+router.get('/login', sessionController.new);//login
+router.post('/login', sessionController.create);//create session
+router.get('/logout', sessionController.destroy);//delete session
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
