4a766c433ff08cb0f352c98a12b793f83f98750e
Daniel Arceo <daniel.arceo@incentro.com>
2015-07-02 16:39:32 +0200

    small changfes on controller

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -26,11 +26,14 @@ exports.create = function(req, res){
 		{ 	texto: req.body.comment.texto,
 			QuizId: req.params.quizId
 		});
-	comment.save().then(function(){
-					res.redirect("/quizes2001/" + req.params.quizId);
-				}, function(err){
-					res.render("comments/new.ejs", {comment:comment, quizid: req.params.quizId, errors: err.errors});
-				});
+	comment.validate().then( function(err) {
+		if (err) {
+			res.render('comments/new.ejs', {comment: comment, quizId: req.params.quizId, errors: err.errors });
+		} else {
+			comment.save().then( function() {
+				res.redirect('/quizes/' + req.params.quizId) })
+		}
+	}).catch( function(error) {next(error)});
 };
 
 // DELETE /quizes/:quizId
