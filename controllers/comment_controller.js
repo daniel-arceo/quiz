73e6c5a5caf1fe751cb9b911c4854845683fe9fa
Daniel Arceo <daniel.arceo@incentro.com>
2015-07-03 09:13:22 +0200

    1

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -27,13 +27,14 @@ exports.create = function(req, res){
 			QuizId: req.params.quizId
 		});
 	comment.validate().then( function(err) {
-		if (err) {
-			res.render('comments/new.ejs', {comment: comment, quizId: req.params.quizId, errors: err.errors });
-		} else {
-			comment.save().then( function() {
-				res.redirect('/quizes/' + req.params.quizId) })
-		}
-	}).catch( function(error) {next(error)});
+        if (err) {
+            res.render('comments/new.ejs', {comment: comment, quizId: req.params.quizId, errors: err.errors });
+        } else {
+            comment.save().then( function() {
+                res.redirect('/quizes/' + req.params.quizId) })
+        }
+    }).catch( function(error) {next(error)});
+
 };
 
 // DELETE /quizes/:quizId
